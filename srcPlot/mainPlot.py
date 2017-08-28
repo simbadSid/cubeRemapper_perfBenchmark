2ba564752b598ab3e8868705d59597eb25a61cd1
kassuskley <kassuskley@pool-210-57-zam2168.wlan.kfa-juelich.de>
2017-08-28 09:06:24 +0200

    Remove plot specification of the Thesis (clean version)

--- srcPlot/mainPlot.py
+++ srcPlot/mainPlot.py
@@ -227,10 +227,10 @@ def plotSurface(X, Y, Z, fig, X_label, Y_label, Z_label):
 
 
 def plotPoint(X, Z, Z_error, fig, ax, X_label, Z_label, legend, barSize, logX, logY, legendExtra="", pointType=0, generateRandomColor=False):
-    if (not legendExtra.startswith("Total")):
-        return
-    if ((legend != 'DEV-SL-trunk') and (legend != 'DEV-SL-AIO') and (legend != 'DEV-SL-AIO-noFalseSharing') and (legend != 'DEV-SL-AIO-noFalseSharing-tcmalloc')): #and (legend != 'DEV-SL-AIO-pthreadWrap')):
-        return
+#    if (not legendExtra.startswith("Write")):
+#        return
+#    if ((legend != 'DEV-SL-trunk') and (legend != 'DEV-SL-AIO') ):# and (legend != 'DEV-SL-AIO-noFalseSharing') and (legend != 'DEV-SL-AIO-noFalseSharing-tcmalloc')): #and (legend != 'DEV-SL-AIO-pthreadWrap')):
+#        return
 
 # TODO to remove
 #    if (legend.startswith("./posixGlibcIO") and legendExtra != "computeTime(0.0001)"):
@@ -271,6 +271,7 @@ def plotPoint(X, Z, Z_error, fig, ax, X_label, Z_label, legend, barSize, logX, l
 
     legend      = fencyLabel(legend)
     legendExtra = fencyLabel(legendExtra)
+#    legendExtra='Write time'
 #    legendExtra='Compute time'
 #    legendExtra='Total time'
     legend = legend + " (" + legendExtra + ")"
